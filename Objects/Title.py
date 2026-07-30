d3695476df024c05d6721467ec620959cfae7a1c
Marmalarmajarma <marcuspoyser@gmail.com>
2026-07-30 11:53:23 +1000

    added Game play room

--- Objects/Title.py
+++ Objects/Title.py
@@ -1,4 +1,5 @@
 from GameFrame import RoomObject
+import pygame
 
 class Title(RoomObject):
     """
@@ -9,3 +10,13 @@ class Title(RoomObject):
 
         image = self.load_image("Title.png")
         self.set_image(image,800,350)
+
+        self.handle_key_events = True
+
+    def key_pressed(self, key):
+        """
+        if the key is pressed the game will start
+        """
+
+        if key[pygame.K_SPACE]:
+            self.room.running = False
