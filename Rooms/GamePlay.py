816d43d63026c3046748bfc5185ee7b146001cc4
Marmalarmajarma <marcuspoyser@gmail.com>
2026-07-30 12:13:08 +1000

    added spaceship object

--- Rooms/GamePlay.py
+++ Rooms/GamePlay.py
@@ -1,8 +1,10 @@
 from GameFrame import Level
+from Objects.Ship import Ship
 
 class GamePlay(Level):
     def __init__(self, screen, joysticks):
         Level.__init__(self, screen, joysticks)
 
         self.set_background_image("Background.png")
-
+        self.add_room_object(Ship(self, 25,50))
+        
